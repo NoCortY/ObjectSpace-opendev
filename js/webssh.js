701defed72e25d2d814e1a6ca103a6c533f9465f
NoCortY <nocorty@163.com>
2020-03-07 21:53:48 +0800

    详细信息(100%) WebSSH(10%)

---
+++ js/webssh.js
@@ -0,0 +1,66 @@
+function WSSHClient() {
+};
+
+WSSHClient.prototype._generateEndpoint = function () {
+    if (window.location.protocol == 'https:') {
+        var protocol = 'wss://';
+    } else {
+        var protocol = 'ws://';
+    }
+    var endpoint = protocol + window.location.host + '/webSSHWS';
+    return endpoint;
+};
+
+WSSHClient.prototype.connect = function (options) {
+    var endpoint = this._generateEndpoint();
+    var base64 = new Base64();
+
+    if (window.WebSocket) {
+        this._connection = new WebSocket(endpoint);
+    }
+    else if (window.MozWebSocket) {
+        this._connection = MozWebSocket(endpoint);
+    }
+    else {
+        options.onError('WebSocket Not Supported');
+        return;
+    }
+
+    this._connection.onopen = function () {
+        options.onConnect();
+    };
+
+    this._connection.onmessage = function (evt) {
+        var data = evt.data.toString();
+        data = base64.decode(data);
+        options.onData(data);
+    };
+
+
+    this._connection.onclose = function (evt) {
+        options.onClose();
+    };
+};
+
+WSSHClient.prototype.send = function (data) {
+    this._connection.send(JSON.stringify(data));
+};
+
+WSSHClient.prototype.sendInitData = function (options) {
+    var data = {
+        hostname: options.host,
+        port: options.port,
+        username: options.username,
+        ispwd: options.ispwd,
+        secret: options.secret
+    };
+    //连接参数
+    this._connection.send(JSON.stringify({"tp": "init", "data": options}))
+}
+
+WSSHClient.prototype.sendClientData = function (data) {
+    //发送指令
+    this._connection.send(JSON.stringify({"tp": "client", "data": data}))
+}
+
+var client = new WSSHClient();
